c0008ec13dd457bef930d3aafe4db4a66141f395
jayforgood <jayforgood@naver.com>
2023-02-18 21:06:15 +0900

    feat: add constants file

---
+++ src/assets/js/dummy/constants.js
@@ -0,0 +1,14 @@
+export const LIST = ["Intro", "Projects", "Contact"];
+
+export const PROJECT_LIST = [
+  {
+    id: 0,
+    title: "title",
+    gitHubUrl: "gitHubUrl",
+    siteUrl: "siteUrl",
+    role: "role",
+    desc: "desc",
+    front: "front",
+    back: "back",
+  },
+];
