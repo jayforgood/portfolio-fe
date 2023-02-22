93575e81490d503561fb9a83cec8af3d86e33bc7
jayforgood <jayforgood@naver.com>
2023-02-22 22:51:03 +0900

    chore : Change style

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -1,14 +1,33 @@
 import React, { Component } from "react";
+import { Breadcrumb } from "antd";
+import { Layout } from "antd";
+
+const { Content } = Layout;
 
 class Intro extends Component {
   state = {};
 
   render() {
     return (
-      <div>
-        <p>Intro</p>
-        안녕하세요. 프론트엔드 개발자 이재훈 입니다.
-      </div>
+      <>
+        <Breadcrumb
+          style={{
+            margin: "16px 0",
+          }}
+        >
+          <Breadcrumb.Item>Intro</Breadcrumb.Item>
+        </Breadcrumb>
+        <Content
+          style={{
+            padding: 24,
+            margin: 0,
+            minHeight: 280,
+            // background: "#dbdbdb",
+          }}
+        >
+          안녕하세요. 프론트엔드 개발자 이재훈 입니다.
+        </Content>
+      </>
     );
   }
 }
